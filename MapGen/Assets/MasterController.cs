df330feb10829a892e2ca81514cefed76db954c7
James LeVasseur <Kamino@Jamess-MacBook-Pro.local>
2015-01-10 19:05:56 -0500

    added unity project and scripts

---
+++ MapGen/Assets/MasterController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterController : MonoBehaviour {
+	GameObject sphere;
+	// Use this for initialization
+	void Start () {
+		GameObject testRegion = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		testRegion.AddComponent<mapgen> ();
+		/*sphere = GameObject.Find("Sphere");
+		for (int i = 0; i < sphere.GetComponent<MeshFilter>().mesh.normals.Length; i++) {
+			Debug.Log ( sphere.GetComponent<MeshFilter>().mesh.normals[i] );
+		}*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ MapGen/Assets/mapgen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mapgen : MonoBehaviour {
+	public Vector3[] newVertices;
+	public Vector2[] newUV;
+	public int[] newTriangles;
+	public Vector3[] newNormals;
+	private GameObject sample;
+	void Start() {
+
+		Mesh mesh = new Mesh ();
+		GetComponent<MeshFilter>().mesh = mesh;
+
+		Vector3 center = Vector3.zero;
+
+		int points = 3;
+
+		newVertices = new Vector3[] { center, new Vector3(0,1,0), new Vector3(1,0,0), new Vector3(0,-1,0), new Vector3(-1,0,0) };
+		newUV = new Vector2[] { Vector2.zero,Vector2.zero,Vector2.zero,Vector2.zero,Vector2.zero };
+		//newNormals = new Vector3[] { new Vector3(-0.2f,-1.0f,-0.1f), new Vector3(-0.1f,-1.0f,-0.1f), new Vector3(-0.1f,-1.0f,-0.1f), new Vector3(-0.0f,-1.0f,-0.2f), new Vector3(-0.0f,-1.0f,-0.2f) };
+		newTriangles = new int[] { 0,1,2,0,2,3 };
+
+		mesh.vertices = newVertices;
+		mesh.uv = newUV;
+		mesh.triangles = newTriangles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
